9680928a98e763a4eb96f5888ae96c67070b0749
RAJARAJAN JAYACHANDRAN <maaxconsultancy@gmail.com>
2026-01-07 21:25:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Live headlines + keyword search using GNews.io API
 // Sign up at https://gnews.io/ and replace YOUR_API_KEY_HERE with your key.
 
-const API_KEY = "  68742387446930499fa5eecb83728eb8";
+const API_KEY ="68742387446930499fa5eecb83728eb8";
 const BASE_URL = "https://gnews.io/api/v4";
 
 async function fetchNews(endpoint, query, container) {
